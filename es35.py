b65684754196fb04ff37d65e8191f0e4e2373454
profPiccioTpsit <151032225+profPiccioTpsit@users.noreply.github.com>
2024-02-22 11:51:51 +0100

    Update es35.py

--- es35.py
+++ es35.py
@@ -1,3 +1,9 @@
+"""
+Data la lista ["ciao","cane","s", "gatto", "paperino" ] , 
+1)creare una seconda lista che contenga la lunghezza delle singole parole della prima lista. 
+2) Stampare a video se esiste almeno  una parola di lunghezza uguale alle  altre 
+"""
+
 lista = ["ciao","cane","s","gatto", "paperino"]
 lista2= []
 
